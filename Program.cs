fcfcfca3d54dc56dc24deaddd5850cba561b581a
brenogcota <brenocota4@gmail.com>
2020-06-25 02:34:43 -0300

    create repository

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace web_mvc
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
