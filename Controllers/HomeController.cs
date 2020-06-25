15183fbd267e615d4909a85bf72ca709f7106371
brenogcota <brenocota4@gmail.com>
2020-06-25 04:06:15 -0300

    create view Departments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using web_mvc.Models;
+using web_mvc.Models.ViewModels;
 
 namespace web_mvc.Controllers
 {
